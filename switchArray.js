a8f8d1b812889f28149cb495ac9409bc9deab724
mr_Green <xxxxx_07@inbox.ru>
2019-01-30 20:28:02 +0300

    add function for switchArray

---
+++ switchArray.js
@@ -0,0 +1,40 @@
+/**
+	@description сдвиг массива на указанное число
+	@param switch число на которое сдвигать массив
+	@param array array входной массив для сдвига
+	@return array
+*/
+
+function switchArray(array, switchCount) {
+	let length = array.length - 1;
+	let direction = (switchCount > 0) ? true : false
+	switchCount = Math.abs(switchCount);
+	while(switchCount){
+
+		if (direction) {
+			let lastValue = array[length];
+			for (let i = length; i > 0 ; i--) {
+				array[i] = array[i - 1];
+			}
+			array[0] = lastValue;
+		}else {
+			let firstValue = array[0];
+			for (let i = 0; i < length ; i++) {
+				array[i] = array[i + 1];
+			}
+			array[length] = firstValue;
+		}
+
+		switchCount--
+	}
+	return array
+}
+
+
+
+let arrayToSwitch = [0,1,2,3,4];
+
+
+let arrayAfterSwitch =  switchArray(arrayToSwitch, 2);
+
+console.log(arrayAfterSwitch);
