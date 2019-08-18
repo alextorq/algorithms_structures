4a0a077f17349debef0615fe1a6dc4859780d039
mr_Green <xxxxx_07@inbox.ru>
2019-08-18 14:27:41 +0300

    add fix stack

--- stack.js
+++ stack.js
@@ -3,34 +3,22 @@
 class Stack {
 
   constructor() {
-      this._count  = 0;
       this._list = [];
   }
 
   push(item) {
     this._list.push(item);
-    this._count++;
   }
   
   pop() {
-      if (!this.count) {
+      if (!this._list.length) {
         return new Error('stack is empty')
       }
       return this._list.pop();
-      this._count--
-  }
-
-  peek() {
-    if (!this.count) {
-      return new Error('stack is empty')
-    }
-    return this._list(this._count - 1);
   }
 
   count() {
-    return this._count;
+    return this._list.length;
   }
-
-
 
 }
